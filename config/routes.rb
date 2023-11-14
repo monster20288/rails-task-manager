00e4c642a4bd26849cb641182d17a438157ebec1
manojvasava <manojvasava5uk@gmail.com>
2023-11-14 12:14:40 +0000

    I can view the details of a task On click

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  #Show List of Tasks
+  get "tasks", to: "tasks#index"
+  # Show List by id of Tasks
+  get "tasks/:id", to: "tasks#show", as: :taskurl
 end
